aa6b2cebe24996c68ebba5fb2824d2cfb6a475b4
Demoincc <marcelltzaras@gmail.com>
2026-03-11 18:10:46 -0400

    Added UFO Gamemode

---
+++ Assets/Scripts/UFOMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class UFOMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [SerializeField] float jumpForce = 8f;
+
+
+    void Start()
+    {
+        rb.freezeRotation = true;
+        rb.gravityScale = 5;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+      
+    }
+}
