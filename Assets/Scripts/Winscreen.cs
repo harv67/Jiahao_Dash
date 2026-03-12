855c14a57ed255736ced454b082c269adc4292ba
Jia-Hao-ZHU <jiahao.zhu26@ycdsbk12.ca>
2026-03-12 11:57:43 -0400

    Levelcomplete panel screen

---
+++ Assets/Scripts/Winscreen.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject winUI; 
+
+    public void ShowWinScreen()
+    {
+        winUI.SetActive(true); 
+        Time.timeScale = 0f;    
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;    
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+        Debug.Log("Quit button pressed!");
+    }
+}
